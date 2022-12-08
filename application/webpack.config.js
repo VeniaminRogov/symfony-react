df1ddbff10c5d114cfcff1b342b5b00028a5c807
Veniamin <rogov.veniamin@gmail.com>
2022-12-08 17:30:41 +0700

    add node into docker

--- application/webpack.config.js
+++ application/webpack.config.js
@@ -63,14 +63,13 @@ Encore
     //.enableTypeScriptLoader()
 
     // uncomment if you use React
-    //.enableReactPreset()
+    .enableReactPreset();
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+//.enableIntegrityHashes(Encore.isProduction())
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
-;
+// uncomment if you're having problems with a jQuery plugin
+//.autoProvidejQuery()
 
 module.exports = Encore.getWebpackConfig();
--- docker/development/node/etc/node-entrypoint.sh
+++ docker/development/node/etc/node-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 if [ "$1" = 'node' ]; then
+    # yarn add @babel/preset-react@^7.0.0 --dev
     yarn install
     yarn watch
 fi
